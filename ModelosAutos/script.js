c31f099ca44998af7721ec1fbc4f172b6fb91738
imnotrichi <ricardo.gutierrez247398@potros.itson.edu.mx>
2025-10-23 00:13:50 -0700

    Initial commit

---
+++ ModelosAutos/script.js
@@ -0,0 +1,46 @@
+const marcasSelect = document.getElementById('marcas');
+const selectContainer = document.getElementById('select-container');
+const modelosSelect = document.getElementById('modelos');
+
+fetch('http://localhost:8888/marcas')
+    .then(response => response.text())
+    .then(data => {
+        const marcas = data.split('\n');
+        marcas.forEach(marca => {
+            const option = document.createElement('option');
+            option.value = marca;
+            option.textContent = marca;
+            marcasSelect.appendChild(option);
+        });
+
+        marcasSelect.addEventListener('change', () => {
+            const selectedMarca = marcasSelect.value;
+            cargarModelos(selectedMarca);
+        });
+    });
+
+function cargarModelos(marca) {
+    fetch(`http://localhost:8888/modelos/${marca}`)
+        .then(response => response.text())
+        .then(data => {
+            const modelos = data.split('\n');
+            modelosSelect.innerHTML = '<option value="">Seleccion un modelo</option>';
+            modelos.forEach(modelo => {
+
+                const option = document.createElement('option');
+                option.value = modelo;
+                option.textContent = modelo;
+
+                const marcaSinEspacios = marca.trim();
+                option.classList.add(`is-${marcaSinEspacios}`);
+                modelosSelect.appendChild(option);
+            });
+        });
+}
+
+modelosSelect.addEventListener('change', () => {
+    const modelo = modelosSelect.value;
+    if (modelo) {
+        alert(`Modelo seleccionado: ${modelo}`);
+    }
+});
